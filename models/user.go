933c61cada4211f389c8df31dcdc8eef756ac1ac
shahriar <shahriarksaurov@gmail.com>
2024-07-14 19:37:10 +0200

    login

--- models/user.go
+++ models/user.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"errors"
+
 	"githib.com/ShahSau/Ticketr/db"
 	"githib.com/ShahSau/Ticketr/utils"
 )
@@ -39,6 +41,26 @@ func (u User) Save() error {
 	}
 
 	u.ID = userId
+
+	return nil
+}
+
+func (u User) Authenticate() error {
+	query := "SELECT id, password FROM users WHERE email = ?"
+	row := db.DB.QueryRow(query, u.Email)
+
+	var retrievedPassword string
+	err := row.Scan(&u.ID, &retrievedPassword)
+
+	if err != nil {
+		return errors.New("Credentials invalid")
+	}
+
+	passwordIsValid := utils.CheckPasswordHash(u.Password, retrievedPassword)
+
+	if !passwordIsValid {
+		return errors.New("Credentials invalid")
+	}
 
 	return nil
 }
--- routes/users.go
+++ routes/users.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"fmt"
 	"net/http"
 
 	"githib.com/ShahSau/Ticketr/models"
@@ -13,16 +14,38 @@ func signup(c *gin.Context) {
 	err := c.ShouldBindJSON(&user)
 
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
 		return
 	}
 
 	err = user.Save()
 
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save user"})
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to save user"})
 		return
 	}
 
 	c.JSON(http.StatusCreated, gin.H{"message": "User created successfully"})
+}
+
+func login(c *gin.Context) {
+	var user models.User
+
+	err := c.ShouldBindJSON(&user)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid request"})
+		return
+	}
+
+	err = user.Authenticate()
+
+	fmt.Println(err, "DDDDD")
+
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"message": "Invalid credentials"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "User logged in successfully"})
 }
